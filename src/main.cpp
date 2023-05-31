7e7d630cd7428c0337cd4db3ccaa42ae74219e3a
Stefan Strableg <darkspark65@gmail.com>
2023-05-31 14:01:19 +0200

    Version 3

--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,8 @@ int main(void)
     Vector2<float> v1(3.f, 4.f);
     Vector2<float> v2(4.f, 3.f);
 
+    std::cout << v1.length() << "\n";
+    std::cout << v1.angle() << "(" << DEG(v1.angle()) << ")\n";
+
     return EXIT_SUCCESS;
 }
