13e6f1440cbdcce760bebe09e2edf22fa5ddf9c9
Stefan Strableg <darkspark65@gmail.com>
2023-08-02 15:07:22 +0200

    Cleaning up the Interface

--- inc/Vector2.hpp
+++ inc/Vector2.hpp
@@ -6,6 +6,7 @@
 #include <type_traits>
 #include <algorithm>
 #include <iostream>
+#include <stdexcept>
 
 #include "Angle.hpp"
 
@@ -15,14 +16,14 @@ struct Vector2
     T x, y;
 
     /// @brief Default constructor
-    inline Vector2()
+    constexpr Vector2()
         : x(), y()
     {
     }
 
     /// @brief Single Value Constructor
     /// @param xy_ Value for both x and y
-    inline Vector2(T xy_)
+    constexpr Vector2(T xy_)
         : x(xy_), y(xy_)
     {
     }
@@ -30,44 +31,44 @@ struct Vector2
     /// @brief Parameterized constructor
     /// @param x_ Value for x
     /// @param y_ Value for y
-    inline Vector2(T x_, T y_)
+    constexpr Vector2(T x_, T y_)
         : x(x_), y(y_)
     {
     }
 
     /// @brief Constructor from std::pair
     /// @param p
-    inline Vector2(const std::pair<T, T> &p)
+    constexpr Vector2(const std::pair<T, T> &p)
         : x(p.first), y(p.second)
     {
     }
 
     /// @brief Move constructor from pair
-    inline Vector2(std::pair<T, T> &&p)
+    constexpr Vector2(std::pair<T, T> &&p)
         : x(std::move(p.first)), y(std::move(p.second))
     {
     }
 
     /// @brief Copy constructor
-    inline Vector2(const Vector2 &v) = default;
+    constexpr Vector2(const Vector2 &v) = default;
 
     /// @brief Move constructor
-    inline Vector2(Vector2 &&v) = default;
+    constexpr Vector2(Vector2 &&v) = default;
 
     /// @brief Returns the angle of the Vector2
-    [[nodiscard]] inline Angle getAngle() const
+    [[nodiscard]] constexpr Angle getAngle() const
     {
         return radians(std::atan2(y, x));
     }
 
     /// @brief Returns the length of the Vector2
-    [[nodiscard]] inline T getLength() const
+    [[nodiscard]] constexpr T getLength() const
     {
         return std::sqrt(x * x + y * y);
     }
 
-    /// @brief Chainable. Sets the angle of the Vector2 while leaving the length unchanged
-    inline Vector2<T> setAngle(Angle ang)
+    /// @brief Sets the angle of the Vector2 while leaving the length unchanged
+    constexpr Vector2<T> &setAngle(Angle ang)
     {
         double len = getLength();
 
@@ -77,8 +78,8 @@ struct Vector2
         return *this;
     }
 
-    /// @brief Chainable. Sets the length of the Vector2 while leaving the angle unchanged
-    inline Vector2<T> setLength(double len)
+    /// @brief Sets the length of the Vector2 while leaving the angle unchanged
+    constexpr Vector2<T> &setLength(double len)
     {
         Angle ang = getAngle();
 
@@ -88,79 +89,75 @@ struct Vector2
         return *this;
     }
 
-    /// @brief Chainable. Swaps the x and y components of the vector
-    [[nodiscard]] inline Vector2<T> swap() const
+    /// @brief Swaps the x and y components of the vector
+    constexpr Vector2<T> &swap()
     {
         std::swap(x, y);
         return *this;
     }
 
-    /// @brief Chainable. Returns the unit vector (Vector with length 1)
-    inline Vector2<T> unit() const
+    /// @brief Returns the unit vector (Vector with length 1)
+    constexpr Vector2<T> &unify()
     {
-        Vector2<T> ret(*this);
-        ret.setLength(1.f);
-        return ret;
+        const T len = getLength();
+        x /= len;
+        y /= len;
+        return *this;
     }
 
-    /// @brief Chainable. Returns a by ang rotated copy of the vector
-    inline Vector2<T> rotate(Angle ang) const
+    /// @brief rotates the vector by ang
+    constexpr Vector2<T> &rotate(Angle ang)
     {
         const float cosine = cos(ang.getRadians());
         const float sine = sin(ang.getRadians());
 
-        return Vector2<T>(
-            cosine * x - sine * y,
-            sine * x + cosine * y);
+        const T _x = x;
+
+        x = cosine * x - sine * y;
+        y = sine * _x + cosine * y;
+
+        return *this;
     }
 
-    /// @brief Chainable. Returns an offset copy of the vector by a translation vector
-    inline Vector2<T> translate(Vector2<T> offset) const
+    /// @brief Translates the vector by another vector
+    constexpr Vector2<T> &translate(Vector2<T> offset)
     {
-        return *this + offset;
+        (*this) += offset;
+        return *this;
     }
 
-    /// @brief Chainable. Returns a copy of the vector multiplied by a factor
-    inline Vector2<T> scale(double factor) const
+    /// @brief Scales the vector by a factor
+    constexpr Vector2<T> &scale(double factor)
     {
-        return *this * Vector2(factor);
-    }
-
-    /// @brief Chainable. Returns a copy of the vector mirrored at an axis
-    template <typename Ta>
-    inline Vector2<T> mirror(Vector2<Ta> axis) const
-    {
-        if (axis.getLength() == 0)
-            return *this;
-
-        axis = axis.rotate(-90).unit();
-        return *this - Vector2(2) * Vector2(dotProduct(*this, axis)) * axis;
+        x *= factor;
+        y *= factor;
+        return *this;
     }
 
     /// @brief Conversion between different Vector2 types
     template <typename To>
-    operator Vector2<To>() const
+    constexpr operator Vector2<To>() const
     {
         return Vector2<To>(static_cast<To>(x), static_cast<To>(y));
     }
 
     /// @brief Conversion to std::pair
-    operator std::pair<T, T>()
+    constexpr operator std::pair<T, T>()
     {
         return std::pair<T, T>(x, y);
     }
 
     /// @brief Copy assignment operator
-    template <typename To>
-    Vector2<T> &operator=(const Vector2<To> &other)
+    constexpr Vector2<T> &operator=(const Vector2<T> &other)
     {
         x = other.x;
         y = other.y;
+        return *this;
     }
 
     /// @brief Move assignment operator
     template <typename To>
-    Vector2<T> &operator=(Vector2<To> &&other)
+    constexpr Vector2<T> &operator=(Vector2<To> &&other)
     {
         x = std::move(other.x);
         y = std::move(other.y);
@@ -170,116 +167,127 @@ struct Vector2
 
 /// @brief Addition operator
 template <typename Ta, typename Tb>
-[[nodiscard]] inline Vector2<typename std::common_type<Ta, Tb>::type> operator+(const Vector2<Ta> &a, const Vector2<Tb> &b)
+[[nodiscard]] constexpr Vector2<typename std::common_type<Ta, Tb>::type> operator+(const Vector2<Ta> &a, const Vector2<Tb> &b)
 {
     return Vector2(a.x + b.x, a.y + b.y);
 }
 
+/// @brief Addition assignment operator
+template <typename Ta, typename Tb>
+constexpr Vector2<Ta> operator+=(Vector2<Ta> &a, const Vector2<Tb> &b)
+{
+    a.x += b.x;
+    a.y += b.y;
+    return a;
+}
+
 /// @brief Subtraction operator
 template <typename Ta, typename Tb>
-[[nodiscard]] inline Vector2<typename std::common_type<Ta, Tb>::type> operator-(const Vector2<Ta> &a, const Vector2<Tb> &b)
+[[nodiscard]] constexpr Vector2<typename std::common_type<Ta, Tb>::type> operator-(const Vector2<Ta> &a, const Vector2<Tb> &b)
 {
     return Vector2(a.x - b.x, a.y - b.y);
 }
 
+/// @brief Subtraction assignment operator
+template <typename Ta, typename Tb>
+constexpr Vector2<Ta> operator-=(Vector2<Ta> &a, const Vector2<Tb> &b)
+{
+    a.x -= b.x;
+    a.y -= b.y;
+    return a;
+}
+
 /// @brief Negation operator
 template <typename Ta>
-[[nodiscard]] inline Vector2<Ta> operator-(const Vector2<Ta> &a)
+[[nodiscard]] constexpr Vector2<Ta> operator-(const Vector2<Ta> &a)
 {
     return Vector2(-a.x, -a.y);
 }
 
 /// @brief Multiplication operator
 template <typename Ta, typename Tb>
-[[nodiscard]] inline Vector2<typename std::common_type<Ta, Tb>::type> operator*(const Vector2<Ta> &a, const Vector2<Tb> &b)
+[[nodiscard]] constexpr Vector2<typename std::common_type<Ta, Tb>::type> operator*(const Vector2<Ta> &a, const Vector2<Tb> &b)
 {
     return Vector2(a.x * b.x, a.y * b.y);
 }
 
+/// @brief Multiplication assignment operator
+template <typename Ta, typename Tb>
+constexpr Vector2<Ta> operator*=(Vector2<Ta> &a, const Vector2<Tb> &b)
+{
+    a.x *= b.x;
+    a.y *= b.y;
+    return a;
+}
+
 /// @brief Division operator
 template <typename Ta, typename Tb>
-[[nodiscard]] inline Vector2<typename std::common_type<Ta, Tb>::type> operator/(const Vector2<Ta> &a, const Vector2<Tb> &b)
+[[nodiscard]] constexpr Vector2<typename std::common_type<Ta, Tb>::type> operator/(const Vector2<Ta> &a, const Vector2<Tb> &b)
 {
+    if (b.x == 0 || b.y == 0)
+        throw std::runtime_error("Division by Zero");
     return Vector2(a.x / b.x, a.y / b.y);
+}
+
+/// @brief Division assignment operator
+template <typename Ta, typename Tb>
+constexpr Vector2<Ta> operator/=(Vector2<Ta> &a, const Vector2<Tb> &b)
+{
+    if (b.x == 0 || b.y == 0)
+        throw std::runtime_error("Division by Zero");
+    a.x /= b.x;
+    a.y /= b.y;
+    return a;
 }
 
 /// @brief Outstream operator
 template <typename T>
-inline std::ostream &operator<<(std::ostream &os, const Vector2<T> &v)
+constexpr std::ostream &operator<<(std::ostream &os, const Vector2<T> &v)
 {
     os << v.x << ',' << v.y;
     return os;
 }
 
-/// @brief Less operator
-template <typename Ta, typename Tb>
-inline std::ostream &operator<(const Vector2<Ta> &a, const Vector2<Tb> &b)
-{
-    return a.getLength() < b.getLength();
-}
-
-/// @brief Greater operator
-template <typename Ta, typename Tb>
-inline std::ostream &operator>(const Vector2<Ta> &a, const Vector2<Tb> &b)
-{
-    return a.getLength() > b.getLength();
-}
-
-/// @brief Less-or-equal operator
-template <typename Ta, typename Tb>
-inline std::ostream &operator<=(const Vector2<Ta> &a, const Vector2<Tb> &b)
-{
-    return a.getLength() <= b.getLength();
-}
-
-/// @brief Greater-or-equal operator
-template <typename Ta, typename Tb>
-inline std::ostream &operator>=(const Vector2<Ta> &a, const Vector2<Tb> &b)
-{
-    return a.getLength() >= b.getLength();
-}
-
 /// @brief Equality operator
 template <typename Ta, typename Tb>
-inline std::ostream &operator==(const Vector2<Ta> &a, const Vector2<Tb> &b)
+constexpr std::ostream &operator==(const Vector2<Ta> &a, const Vector2<Tb> &b)
 {
     return (a.x == b.x) && (a.y == b.y);
 }
 
 /// @brief Inequality operator
 template <typename Ta, typename Tb>
-inline std::ostream &operator!=(const Vector2<Ta> &a, const Vector2<Tb> &b)
+constexpr std::ostream &operator!=(const Vector2<Ta> &a, const Vector2<Tb> &b)
 {
     return (a.x != b.x) || (a.y != b.y);
 }
 
 /// @brief Dot product of two vectors
 template <typename Ta, typename Tb>
-[[nodiscard]] inline double dotProduct(const Vector2<Ta> &a, const Vector2<Tb> &b)
+[[nodiscard]] constexpr double dotProduct(const Vector2<Ta> &a, const Vector2<Tb> &b)
 {
     return a.x * b.x + a.y * b.y;
 }
 
 /// @brief Cross Product of two vectors
 template <typename Ta, typename Tb>
-[[nodiscard]] inline double crossProduct(const Vector2<Ta> &a, const Vector2<Tb> &b)
+[[nodiscard]] constexpr double crossProduct(const Vector2<Ta> &a, const Vector2<Tb> &b)
 {
     return a.x * b.y - a.y * b.x;
 }
 
 /// @brief Projects one vector onto another
 template <typename Ta, typename Tb>
-inline Vector2<Tb> project(const Vector2<Ta> &v, const Vector2<Tb> &onto)
+[[nodiscard]] constexpr Vector2<Tb> project(const Vector2<Ta> &v, const Vector2<Tb> &onto)
 {
     return Vector2(dotProduct(v, onto) / (onto.getLength() * onto.getLength())) * onto;
 }
 
-// Ostiiiii
 /// @brief Rejects one vector from another
 template <typename Ta, typename Tb>
-[[deprecated("This does not work yet!")]] inline Vector2<Tb> reject(const Vector2<Ta> &v, const Vector2<Tb> &from)
+[[nodiscard]] constexpr Vector2<Tb> reject(const Vector2<Ta> &v, const Vector2<Tb> &from)
 {
-    return project(v, from.rotate(90));
+    return v - project(v, from);
 }
 
 /// @brief Commonly used interpolations
@@ -287,14 +295,14 @@ namespace Interpolation
 {
     /// @brief Linear Interpolation between two vectors
     template <typename Ta, typename Tb>
-    inline Vector2<typename std::common_type<Ta, Tb>::type> linear(Vector2<Ta> a, Vector2<Tb> b, double t)
+    [[nodiscard]] constexpr Vector2<typename std::common_type<Ta, Tb>::type> linear(Vector2<Ta> a, Vector2<Tb> b, double t)
     {
         return a + Vector2(t) * (b - a);
     }
 
     /// @brief Smoothstep Interpolation between two vectors
     template <typename Ta, typename Tb>
-    inline Vector2<typename std::common_type<Ta, Tb>::type> smoothstep(Vector2<Ta> a, Vector2<Tb> b, double t)
+    [[nodiscard]] constexpr Vector2<typename std::common_type<Ta, Tb>::type> smoothstep(Vector2<Ta> a, Vector2<Tb> b, double t)
     {
         t = std::clamp(t, 0.0, 1.0);
 
--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,102 @@
 
 int main(void)
 {
-    Vector2 v(9.f, 9.f);
+    Vector2 a(2.f, 4.f);
+    Vector2f b;
+    Vector2 c(a);
 
-    std::cout << v.rotate(degrees(-90)) << '\n';
+    std::cout << " CONSTRUCTION:\n\n";
+    std::cout << "a: (" << a << ") or len "
+              << a.getLength() << " ang " << a.getAngle().getDegrees()
+              << ",\n\tb: (" << b << ") or len "
+              << b.getLength() << " ang " << b.getAngle().getDegrees()
+              << ",\n\tc: (" << c << ") or len "
+              << c.getLength() << " ang " << c.getAngle().getDegrees()
+              << '\n';
+
+    b.setLength(a.getLength());
+    b.setAngle(a.getAngle());
+
+    std::cout << "\n SET LENGTH AND ANGLE:\n\n";
+    std::cout << "a: (" << a << ") or len "
+              << a.getLength() << " ang " << a.getAngle().getDegrees()
+              << ",\n\tb: (" << b << ") or len "
+              << b.getLength() << " ang " << b.getAngle().getDegrees()
+              << '\n';
+
+    a.swap();
+    b.unify();
+
+    std::cout << "\n SWAP AND UNIFY:\n\n";
+    std::cout << "a: (" << a << ") or len "
+              << a.getLength() << " ang " << a.getAngle().getDegrees()
+              << ",\n\tb: (" << b << ") or len "
+              << b.getLength() << " ang " << b.getAngle().getDegrees()
+              << '\n';
+
+    a.rotate(degrees(90));
+    b.rotate(degrees(-45));
+
+    std::cout << "\n ROTATE:\n\n";
+    std::cout << "a: (" << a << ") or len "
+              << a.getLength() << " ang " << a.getAngle().getDegrees()
+              << ",\n\tb: (" << b << ") or len "
+              << b.getLength() << " ang " << b.getAngle().getDegrees()
+              << '\n';
+
+    a.translate(Vector2(2, -2));
+    b.scale(4);
+
+    std::cout << "\n TRANSLATE AND SCALE:\n\n";
+    std::cout << "a: (" << a << ") or len "
+              << a.getLength() << " ang " << a.getAngle().getDegrees()
+              << ",\n\tb: (" << b << ") or len "
+              << b.getLength() << " ang " << b.getAngle().getDegrees()
+              << '\n';
+
+    a = Vector2(2, 2);
+    b = Vector2(4, 0);
+
+    std::cout << "\n RESET TO CLEAR VALUES:\n\n";
+    std::cout << "a: (" << a << ") or len "
+              << a.getLength() << " ang " << a.getAngle().getDegrees()
+              << ",\n\tb: (" << b << ") or len "
+              << b.getLength() << " ang " << b.getAngle().getDegrees()
+              << '\n';
+
+    auto _a = project(a, b);
+    auto _b = reject(a, b);
+
+    std::cout << "\n PROJECTION AND RECTION OF A ONTO/FROM B:\n\n";
+    std::cout << "a: (" << _a << ") or len "
+              << _a.getLength() << " ang " << _a.getAngle().getDegrees()
+              << ",\n\tb: (" << _b << ") or len "
+              << _b.getLength() << " ang " << _b.getAngle().getDegrees()
+              << '\n';
+
+    std::cout << "\n CROSS AND DOT PRODUCT:\n\n";
+    std::cout << "a x b: " << crossProduct(a, b) << "\n"
+              << "a . b: " << dotProduct(a, b) << "\n";
+
+    std::cout << "\n ARITHMETICS:\n\n";
+    std::cout << "a + b = " << a << " + " << b << " = " << (a + b) << "\n";
+    std::cout << "a - b = " << a << " - " << b << " = " << (a - b) << "\n";
+    std::cout << "a * b = " << a << " * " << b << " = " << (a * b) << "\n";
+    std::cout << "b / a = " << b << " / " << a << " = " << (b / a) << " (b/a because no division by zero)\n";
+
+    std::cout << "\n ASSIGNMENT OPERATORS:\n\n";
+    c += b;
+    std::cout << "c += b; " << c << " += " << b << " = " << c << "\n";
+    c -= b;
+    std::cout << "c -= b; " << c << " -= " << b << " = " << c << "\n";
+    c *= b;
+    std::cout << "c *= b; " << c << " *= " << b << " = " << c << "\n";
+    c /= a;
+    std::cout << "c /= a; " << c << " /= " << a << " = " << c << "\n";
+
+    b = a;
+
+    std::cout << "\n LOGIC OPERATORS:\n\n";
 
     return EXIT_SUCCESS;
 }
